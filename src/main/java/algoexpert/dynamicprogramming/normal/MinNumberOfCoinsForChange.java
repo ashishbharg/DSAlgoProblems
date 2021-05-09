128c56dad229d6e17b8c7de95c090d91cceb08cd
Ashish Bhargava <ashishbharg@gmail.com>
2021-05-09 12:14:20 +0530

    Find minimum number of coins required to make a change

--- src/main/java/algoexpert/dynamicprogramming/normal/MinNumberOfCoinsForChange.java
+++ src/main/java/algoexpert/dynamicprogramming/normal/MinNumberOfCoinsForChange.java
@@ -23,22 +23,18 @@ public class MinNumberOfCoinsForChange {
     public static void main(String[] args) {
         int[] denoms = {1, 5, 10};
         int minNumberOfCoinsForChange = minNumberOfCoinsForChange(7, denoms);
-        System.out.println(minNumberOfCoinsForChange);
         assert 3 == minNumberOfCoinsForChange;
 
         denoms = new int[] {2, 1};
         minNumberOfCoinsForChange = minNumberOfCoinsForChange(3, denoms);
-        System.out.println(minNumberOfCoinsForChange);
         assert 2 == minNumberOfCoinsForChange;
 
         denoms = new int[] {2, 4};
         minNumberOfCoinsForChange = minNumberOfCoinsForChange(7, denoms);
-        System.out.println(minNumberOfCoinsForChange);
         assert -1 == minNumberOfCoinsForChange;
 
         denoms = new int[] {39, 45, 130, 40, 4, 1};
         minNumberOfCoinsForChange = minNumberOfCoinsForChange(135, denoms);
-        System.out.println(minNumberOfCoinsForChange);
         assert 3 == minNumberOfCoinsForChange;
     }
 
