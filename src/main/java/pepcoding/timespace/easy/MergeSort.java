72369880827987b12ed37c5a54a5eb339e75fa1b
Ashish Bhargava <ashishbharg@gmail.com>
2021-08-24 23:05:00 +0530

    Merge Sort

--- src/main/java/pepcoding/timespace/easy/MergeSort.java
+++ src/main/java/pepcoding/timespace/easy/MergeSort.java
@@ -1,5 +1,9 @@
 package pepcoding.timespace.easy;
 
+/**
+ * 1. You are given an array(arr) of integers.
+ * 2. You have to sort the given array in increasing order using the merge sort.
+ */
 public class MergeSort {
 
     public static void main(String[] args) {
